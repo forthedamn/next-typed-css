4de05a3a6b5031d1fb3ef43f6b857016390fbbe2
forthedamn <zhoguoxin@126.com>
2018-08-08 22:42:53 +0800

    init

---
+++ css-loader-config.js
@@ -0,0 +1,61 @@
+const findUp = require('find-up')
+
+module.exports = (
+  config,
+  extractPlugin,
+  { cssModules = false, cssLoaderOptions = {}, postcssLoaderOptions = {}, dev, isServer, tsCssModules, loaders = [] }
+) => {
+  const postcssConfig = findUp.sync('postcss.config.js', {
+    cwd: config.context
+  })
+  let postcssLoader
+
+  if (postcssConfig) {
+    //Copy the postcss-loader config options first.
+    postcssOptionsConfig = Object.assign(
+      {},
+      postcssLoaderOptions.config,
+      { path: postcssConfig }
+    )
+
+    postcssLoader = {
+      loader: 'postcss-loader',
+      options: Object.assign(
+        {},
+        postcssLoaderOptions,
+        { config: postcssOptionsConfig }
+      )
+    }
+  }
+
+  const cssLoader = {
+    loader: isServer ? 'css-loader/locals' : (tsCssModules ? 'typings-for-css-modules-loader' : 'css-loader'),
+    options: Object.assign(
+      {},
+      {
+        modules: tsCssModules || cssModules,
+        minimize: !dev,
+        sourceMap: dev,
+        importLoaders: loaders.length + (postcssLoader ? 1 : 0)
+      },
+      cssLoaderOptions
+    )
+  }
+
+  // When not using css modules we don't transpile on the server
+  if (isServer && !cssLoader.options.modules) {
+    return ['ignore-loader']
+  }
+
+  // When on the server and using css modules we transpile the css
+  if (isServer && cssLoader.options.modules) {
+    return [cssLoader, postcssLoader, ...loaders].filter(Boolean)
+  }
+
+  return [
+    dev && 'extracted-loader',
+    ...extractPlugin.extract({
+      use: [cssLoader, postcssLoader, ...loaders].filter(Boolean)
+    })
+  ].filter(Boolean)
+}
---
+++ readme.md
@@ -0,0 +1,43 @@
+# Next.js + CSS + Typescript
+
+Import `.css` files in your Next.js project with TypeScript typings
+
+Fork from @zeit/next-css but typing with Typescript.
+
+## Installation
+
+```
+npm install --save next-typed-css
+```
+
+or
+
+```
+yarn add next-typed-css
+```
+
+## Usage
+
+Most config same as [@zeit/next-css](https://github.com/zeit/next-plugins/tree/master/packages/next-css)
+
+
+## Difference
+
+### Use typings CSS with Typescript
+
+Typing css based on [typings-for-css-modules-loader](https://github.com/Jimdo/typings-for-css-modules-loader)
+
+```
+// next.config.js
+const withCSS = require('next-typed-css')
+module.exports = withCSS({
+  // no need to set 'cssModule: true' again
+  tsCssModules: true
+  cssLoaderOptions: {
+    // typings-for-css-modules-loader config here
+    namedExport: true
+  }
+})
+```
+
+
